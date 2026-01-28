b66207e532da95cf51edaff7020739c304428e28
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2026-01-28 10:09:37 -0500

    Code to save a puzzle

---
+++ save_puzzle.py
@@ -0,0 +1,172 @@
+
+from crossword import Crossword
+import puz
+
+puzzle= [
+['L', 'A', 'U', 'R', 'E', 'N', '.', 'L', 'I', 'B'],
+['A', 'R', 'S', 'E', 'N', 'O', '.', 'A', 'C', 'T'],
+['G', 'O', 'T', 'E', 'A', 'M', '.', 'S', 'E', 'W'],
+['S', 'D', 'A', '.', 'M', '.', 'H', 'I', 'D', '.'],
+['.', '.', '.', 'D', 'E', 'R', 'E', 'K', '.', '.'],
+['.', '.', 'S', 'O', 'L', 'A', 'R', '.', '.', '.'],
+['.', 'C', 'A', 'M', '.', 'G', '.', 'B', 'F', 'A'],
+['L', 'O', 'U', '.', 'P', 'T', 'I', 'L', 'E', 'S'],
+['A', 'M', 'C', '.', 'T', 'A', 'R', 'I', 'N', 'I'],
+['Y', 'A', 'Y', '.', 'A', 'G', 'E', 'N', 'D', 'A']
+]
+puzzle= [
+['L', 'A', 'U', 'R', 'E', 'N', '.', 'B', 'S', 'O'],
+['O', 'N', 'T', 'I', 'M', 'E', '.', 'R', 'A', 'W'],
+['O', 'N', 'E', 'G', 'O', 'D', '.', 'O', 'N', 'E'],
+['M', 'E', 'S', 'I', 'T', '.', 'G', 'O', 'D', '.'],
+['.', '.', '.', 'D', 'E', 'R', 'E', 'K', '.', '.'],
+['.', '.', 'E', 'L', 'D', 'E', 'R', '.', '.', '.'],
+['.', 'J', 'A', 'Y', '.', 'P', 'H', 'A', 'S', 'E'],
+['P', 'E', 'T', '.', 'M', 'E', 'A', 'T', 'A', 'L'],
+['A', 'D', 'I', '.', 'T', 'A', 'R', 'I', 'N', 'I'],
+['D', 'I', 'N', '.', 'A', 'T', 'D', 'U', 'K', 'E'] 
+]
+
+puzzle= [
+['L', 'A', 'U', 'R', 'E', 'N', '.', 'R', 'S', 'S'],
+['I', 'N', 'F', 'E', 'S', 'T', '.', 'U', 'N', 'A'],
+['E', 'N', 'A', 'C', 'T', 'S', '.', 'B', 'A', 'D'],
+['N', 'O', 'S', 'E', 'E', '.', 'T', 'I', 'P', '.'],
+['.', '.', '.', 'D', 'E', 'R', 'E', 'K', '.', '.'],
+['.', '.', 'H', 'E', 'M', 'A', 'N', '.', '.', '.'],
+['.', 'H', 'I', 'D', '.', 'M', 'Y', 'G', 'O', 'D'],
+['K', 'I', 'N', '.', 'G', 'O', 'E', 'A', 'S', 'Y'],
+['O', 'L', 'D', '.', 'O', 'N', 'A', 'R', 'U', 'N'],
+['S', 'T', 'U', '.', 'T', 'A', 'R', 'I', 'N', 'A']
+]
+
+puzzle = [
+['O', 'U', 'C', 'H', 'E', 'S', '.', 'E', 'L', 'F'],
+['R', 'E', 'L', 'E', 'N', 'T', '.', 'N', 'E', 'O'],
+['C', 'L', 'E', 'R', 'G', 'Y', '.', 'J', 'U', 'G'],
+['A', 'I', 'O', 'L', 'I', '.', 'R', 'O', 'C', '.'],
+['.', '.', '.', 'I', 'N', 'L', 'A', 'Y', '.', '.'],
+['.', '.', 'C', 'H', 'E', 'A', 'P', '.', '.', '.'],
+['.', 'J', 'A', 'Y', '.', 'T', 'P', 'A', 'I', 'N'],
+['T', 'E', 'R', '.', 'A', 'H', 'E', 'R', 'N', 'E'],
+['U', 'S', 'E', '.', 'P', 'E', 'R', 'M', 'I', 'T'],
+['B', 'T', 'W', '.', 'E', 'R', 'S', 'A', 'T', 'Z']
+]
+
+puzzle = [
+['.', 'J', 'O', 'E', 'Y', 'B', '.', '.', 'O', 'L', 'D', 'A', 'G', 'E', '.'],
+['A', 'U', 'L', 'D', 'E', 'R', '.', '.', 'R', 'E', 'G', 'R', 'I', 'P', '.'],
+['G', 'L', 'I', 'S', 'C', 'O', 'R', '.', 'W', 'U', 'G', 'T', 'R', 'I', 'O'],
+['E', 'I', 'N', '.', 'C', 'M', 'E', 'R', 'E', '.', '.', '.', 'L', 'S', 'D'],
+['M', 'A', 'K', 'U', 'H', 'I', 'T', 'A', '.', '.', 'A', 'S', 'S', 'T', 'D'],
+['.', '.', '.', 'C', 'Y', 'N', 'I', 'C', '.', 'C', 'H', 'I', 'C', 'O', '.'],
+['N', 'B', 'C', 'S', '.', 'E', 'N', 'E', '.', 'B', 'O', 'U', 'L', 'L', 'E'],
+['O', 'O', 'H', '.', 'I', 'G', 'A', '.', 'E', 'S', 'Y', '.', 'U', 'E', 'R'],
+['D', 'I', 'A', 'L', 'G', 'A', '.', 'A', 'D', 'N', '.', 'A', 'B', 'R', 'A'],
+['.', 'L', 'P', 'G', 'A', 'S', '.', 'M', 'I', 'E', 'N', 'S', '.', '.', '.'],
+['M', 'A', 'A', 'R', 'S', '.', '.', 'E', 'T', 'T', 'A', 'K', 'E', 'T', 'T'],
+['D', 'N', 'R', '.', '.', '.', 'K', 'N', 'O', 'W', 'N', '.', 'R', 'O', 'O'],
+['P', 'E', 'R', 'S', 'I', 'A', 'N', '.', 'R', 'O', 'N', 'R', 'I', 'C', 'O'],
+['.', 'G', 'A', 'U', 'L', 'L', 'E', '.', '.', 'R', 'Y', 'O', 'K', 'A', 'N'],
+['.', 'G', 'L', 'A', 'L', 'I', 'E', '.', '.', 'K', 'S', 'T', 'A', 'R', '.'],
+]
+
+xword = Crossword(15, 15, optional_grid=puzzle)
+
+
+p = puz.Puzzle()
+p.width = 15
+p.height = 15
+
+# 1. Convert grid to a flat string. Use '.' for black squares.
+solution_str = "".join(["".join(row) for row in puzzle])
+p.solution = solution_str
+
+# 2. Create the 'fill' (the blank grid for the player)
+# Characters are '-' for playable cells and '.' for blocks
+p.fill = "".join(['-' if c != '.' else '.' for c in solution_str])
+
+# 3. Add Metadata
+p.title = "Gotta write 'em all"
+p.author = "Ryan Mueller"
+
+# 4. Define Clues
+# IMPORTANT: These MUST be in order of the grid numbers (1, 2, 3...)
+# If square 1 has both an Across and Down, the Across clue comes first.
+p.clues = [
+"""Rat Pack member""", 
+ """Child in the kitchen""", 
+ """Bob-___""", 
+ """Typo fixers, in brief""", 
+ """Gross""", 
+ """A dense red vapor""", 
+ """Geriatrics study""", 
+ """"Take hold on me, ___ shall be divided": Shelley""", 
+ """Romania's monetary unit""", 
+ """German recording co.""", 
+ """Works on walls, perhaps""", 
+ """No boys allowed""", 
+ """Letter writer""", 
+ """More senior in Scottland""", 
+ """"Never so rich ___ was set in worse than gold": "The Merchant of Venice" """, 
+ """Fix, in a way, as golf clubs""", 
+ """Scorpion or bat? Why not both!""", 
+ """Site of some cones""", 
+ """Dugtrio look-a-like""", 
+ """Uneven?""", 
+ """Austrian article""", 
+ """Beckoning elision""", 
+ """Track event""", 
+ """It's taken on some trips""", 
+ """Baby Hariyama""", 
+ """Pokemon MOBA tournament series in short""", 
+ """Like gift-box chocolates (Abbr.)""", 
+ """Sailor's shout""", 
+ """Char ___ (Cantonese BBQ pork)""", 
+ """Diogenes, for one""", 
+ """Harpo's big brother""", 
+ """American broadcast television provider""", 
+ """Belonging to a Big Three network""", 
+ """Silently assent""", 
+ """Lesson #1 in cooking""", 
+ """Thicket""", 
+ """U-turn from WSW""", 
+ """"The Bridge Over the River Kwai" writer""", 
+ """Unit in history class""", 
+ """Sound from a fireworks viewer""", 
+ """Tennis champ Swiatek""", 
+ """Alternatives to Krogers and Safeways""", 
+ """Finish for "court" or "her" """, 
+ """Sentence shortener, of a sort""", 
+ """European Broadcasting Union """, 
+ """Pokemon of myth created by Arceus""", 
+ """More sizable, briefly.""", 
+ """Former Mideast British colony: Abbr.""", 
+ """Word of agreement""", 
+ """1974 pop hit "___-Ca-Dabra" """, 
+ """Show curiosity""", 
+ """Fuel in a tank, briefly""", 
+ """Appearances""", 
+ """NANNYS""", 
+ """MAARS""", 
+ """Nasheed's Maldivian party""", 
+ """Comics teenager who debuted in 1925""", 
+ """Alexander who plays Coraline in "American Fiction" """, 
+ """To touch, to Tegucigalpans""", 
+ """Roger Rabbit, for one""", 
+ """Letters of request for some patients""", 
+ """Familiar""", 
+ """Spot for a tot, at times""", 
+ """Friend of Pooh and Eeyore""", 
+ """Language aka Farsi""", 
+ """Latin possessive""", 
+ """Under the weather""", 
+ """Oscar winner Mahershala""", 
+ """Rum brand""", 
+ """Go bad""", 
+ """Gen. Charles de ___""", 
+ """RYOKAN""", 
+ """GLALIE""", 
+ """Orange-to-red star, as Aldebaran""", 
+]
+p.save("my_puzzle.puz")
