ec1b80aa8118980bc0cedbd16cdd0c76ce6ead43
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2026-01-28 10:27:32 -0500

    Code to read clue csv

--- read_clues.py
+++ read_clues.py
@@ -9,12 +9,46 @@ def make_clue_dict():
                 pub = parts[0]
                 year = parts[1]
                 answer = parts[2]
-                clue = parts[3]
-                clue_dict[answer] = (clue, year,pub)
+                clue = parts[3] 
+                try:
+                    year = int(year)
+                except:
+                    year = 0
+                # Keep most recent clue
+                if answer in clue_dict:
+                    if year > clue_dict[answer][1]:
+                        clue_dict[answer] = (clue, year, pub)
+                else:
+                    clue_dict[answer] = (clue, year, pub)
+    return clue_dict
+
+
+
+def read_clue_csv():
+    clue_dict = {}
+    with open('wordlists/clues.csv', 'r') as f:
+        for line in f:
+            parts = line.strip().split(',')
+            if len(parts) >= 4:
+                pub = parts[3]
+                year = parts[2]
+                answer = parts[0]
+                clue = parts[1] 
+                try:
+                    year = int(year)
+                except:
+                    year = 0
+                # Keep most recent clue
+                if answer in clue_dict:
+                    if year > clue_dict[answer][1]:
+                        clue_dict[answer] = (clue, year, pub)
+                else:
+                    clue_dict[answer] = (clue, year, pub)
     return clue_dict
 
 if __name__ == "__main__":
     import pandas as pd
     clue_dict = make_clue_dict()
     df = pd.DataFrame.from_dict(clue_dict, orient='index', columns=['clue', 'year', 'pub'])
-    print(df.pub.unique())
+    df.to_csv("wordlists/clues.csv")
+    print(df.year.mean())
